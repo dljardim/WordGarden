6c9516bb74035a88067c19e4c7cb5cfb9b8b5918
dljardim <dljardim@outlook.com>
2025-02-10 20:38:18 -0600

    Initial Commit

---
+++ WordGarden/WordGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGardenApp.swift
+//  WordGarden
+//
+//  Created by Damian Jardim on 2/10/25.
+//
+
+import SwiftUI
+
+@main
+struct WordGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
